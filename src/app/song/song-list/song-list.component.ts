207776b55716d5116a4fbe5ca399a6e59e75f513
Jef De Smedt <Jef.DeSmedt@dbicts.be>
2021-12-14 10:58:02 +0100

    SongListComponet with Song2Service

--- src/app/song/song-list/song-list.component.ts
+++ src/app/song/song-list/song-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { map } from 'rxjs';
 import { Song } from 'src/app/model/song';
-import { SongService } from 'src/app/services/song.service';
+import { Song2 } from 'src/app/model/song2';
+import { Song2HttpService } from 'src/app/services/song2-http.service';
 
 @Component({
   selector: 'app-song-list',
@@ -8,13 +10,22 @@ import { SongService } from 'src/app/services/song.service';
   styleUrls: ['./song-list.component.css']
 })
 export class SongListComponent implements OnInit {
-
-  constructor(private songService:SongService) { }
+  public songs: Song2[]
+  constructor(private songService:Song2HttpService) {
+    this.songs = new Array<Song2>();
+   }
 
   ngOnInit(): void {
+    this.refresh();
   }
-  get songs():Iterable<Song>{
-    return this.songService;
+  refresh(){
+    this.songService.getSongs().pipe(map(sList => {
+      return sList.map(s => Song2.copySong(s))
+    })).subscribe({
+      next: (pList:Song2[]) => {
+        this.songs = pList;
+      }
+    })
   }
 
 }
