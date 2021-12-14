1005eb4bda7bdb26a2336ae891e0ea0af8be439f
Jef De Smedt <Jef.DeSmedt@dbicts.be>
2021-12-14 07:56:34 +0100

    ProductItemComponent added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-comp',
-  template: '<span>{{ title }}</span>',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
