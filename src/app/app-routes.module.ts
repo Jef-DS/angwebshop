a7058a81ec8d461f6e82761bf9f902c746715f83
Jef De Smedt <Jef.DeSmedt@dbicts.be>
2021-12-15 08:27:45 +0100

    A menu for ProductList

--- src/app/app-routes.module.ts
+++ src/app/app-routes.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ErrorpageComponent } from './pages/errorpage/errorpage.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ErrorpageComponent } from './pages/errorpage/errorpage.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
 
 const appRoutes: Routes= [
   {path:'', redirectTo:"/home", pathMatch: 'full'},
   {path:'home', component:HomepageComponent},
+  {path:'products', component:ProductListComponent},
   {path:'**', component:ErrorpageComponent}
 ]
 
